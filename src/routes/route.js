b6eb57b130d151d3d8faa8c6fd960cf9c5e1efab
Bhuwan Samant <you@example.com>
2022-08-10 13:04:10 +0530

    Quarry parametrs- Assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -88,6 +88,8 @@ router.get("/films/:filmId", function(req, res){
        }]
 
        let filmId = req.params.filmId
+     //   bcz api me dynamic varibal (:) path parametar, req ke parametar ke obj , ke rup me store hota h
+          
 
        //iterate all the films
        //search for a film whose id matches with the id recevied in request
@@ -102,6 +104,77 @@ router.get("/films/:filmId", function(req, res){
        //if there is no match give an error response
        res.send("The film id doesn't match any movie")
 })
+
+
+//////////////  multipal path params ////////////////////////////////////////
+router.get('/name/:a/:b/:c/:d',function(req,res){
+    //  api me dynamic varibal (:) path parametar, req ke parametar ke obj , ke rup me store hota h
+
+   let a=req.params.a
+   let b=req.params.b
+   let c=req.params.c
+   let d=req.params.d
+   let e=req.params.e
+
+   // console.log(a,b,c,d)
+
+   res.send([a,b,c,d])
+   
+   
+})
+
+
+///////////////////////////////// Problem 1 ///////////////////////////////////////////////
+ 
+//    -write an api which gives the missing number in an array of integers starting 
+//    from 1….e.g [1,2,3,5,6,7] : 4 is missing
+
+   router.get("/sol1", function (req, res) {
+    //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
+    let arr= [1,2,3,5,6,7]
+  
+    let sum = 0;
+    for (var i in arr) {
+        total += arr[i];
+    }
+  
+    let lastDigit= arr.pop()
+    let consecutiveSum= lastDigit * (lastDigit+1) / 2
+
+    let missingNumber= consecutiveSum - total
+  
+    res.send(  { data: missingNumber  }  );
+  });
+
+
+    
+///////////////////////////////// Problem 2 //////////////////////////////////////////////////
+
+//  -write an api which gives the missing number in an array of integers starting from 
+//  anywhere….e.g [33, 34, 35, 37, 38]: 36 is missing
+
+ router.get("/sol2", function (req, res) {
+    //logic : sum of n consecutive numbers is [ n * (first + last) / 2  ]..so get sum of all numbers in array. now take sum of n consecutive numbers.. n would be length+1 as 1 number is missing
+    let arr= [33, 34, 35, 37, 38]
+    let len= arr.length
+  
+    let sum = 0;
+    for (var i in arr) {
+        total += arr[i];
+    }
+  
+    let firstDigit= arr[0]
+    let lastDigit= arr.pop()
+
+    let consecutiveSum= (len + 1) * (firstDigit+ lastDigit ) / 2
+
+   0 let missingNumber= consecutiveSum - total
+    
+   
+    res.send(  { data: missingNumber  }  );
+  });
+
+
 
 module.exports = router;
 // adding this comment for no reason
